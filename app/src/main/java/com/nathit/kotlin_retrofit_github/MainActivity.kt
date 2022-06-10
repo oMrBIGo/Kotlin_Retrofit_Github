e9b1d466a90b205dbc0c29014f581c4b8ff4ca83
oMrBIGo <nathit.panrod1@gmail.com>
2022-06-10 18:16:45 +0700

    Nathit

---
+++ app/src/main/java/com/nathit/kotlin_retrofit_github/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.nathit.kotlin_retrofit_github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
